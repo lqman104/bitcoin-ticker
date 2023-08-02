e7ca7084eecf81efca79f74ec44998a801aa169f
Luqman <lqman104@gmail.com>
2023-08-02 19:37:11 +0700

    feat: change getExchangeableValue return value

--- lib/repository/currency_repository.dart
+++ lib/repository/currency_repository.dart
@@ -1,9 +1,13 @@
-import 'package:bitcoin_ticker/service/Network.dart';
+import 'package:bitcoin_ticker/service/network.dart';
 
 class CurrencyRepository {
-
-  static Future<dynamic> getExchangeValue(String from, String to) async {
-    NetworkHelper networkHelper = NetworkHelper(path: "${NetworkHelper.RATE_PATH}/$to/$from");
-    return await networkHelper.get();
+  static Future<String> getExchangeValue({
+    required String from,
+    required String to,
+  }) async {
+    NetworkHelper networkHelper =
+        NetworkHelper(path: "${NetworkHelper.RATE_PATH}/$to/$from");
+    var response = await networkHelper.get();
+    return double.tryParse(response['rate'].toString())?.round().toString() ?? "0";
   }
 }
