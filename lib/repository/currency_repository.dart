dd85edeac8855fbc03f7f3ac87ae9d0a151e4c61
Luqman <lqman104@gmail.com>
2023-08-03 10:01:13 +0700

    revert: return type of the function

--- lib/repository/currency_repository.dart
+++ lib/repository/currency_repository.dart
@@ -1,13 +1,12 @@
 import 'package:bitcoin_ticker/service/network.dart';
 
 class CurrencyRepository {
-  static Future<String> getExchangeValue({
+  static Future<dynamic> getExchangeValue({
     required String from,
     required String to,
   }) async {
     NetworkHelper networkHelper =
         NetworkHelper(path: "${NetworkHelper.RATE_PATH}/$to/$from");
-    var response = await networkHelper.get();
-    return double.tryParse(response['rate'].toString())?.round().toString() ?? "0";
+    return await networkHelper.get();
   }
 }
